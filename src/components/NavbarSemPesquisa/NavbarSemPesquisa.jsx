17bca4278bddc1fb8c230bb3a26dc0af469bf2ac
Gabriel Henrique Lins <130785312+gabrielhilins@users.noreply.github.com>
2024-04-24 00:22:46 -0300

    fazendo as minhas telas
    Sobre: Concluida
    CadastroProdutior: concluida

--- src/components/NavbarSemPesquisa/NavbarSemPesquisa.jsx
+++ src/components/NavbarSemPesquisa/NavbarSemPesquisa.jsx
@@ -1,12 +1,12 @@
 import { Link as RouterLink } from "react-router-dom"
-import { Flex, Box, Link, InputGroup, Input, InputRightElement, IconButton, Text } from "@chakra-ui/react"
-import { BellIcon, ChatIcon, SearchIcon } from "@chakra-ui/icons"
+import { Flex, Box, Link, Text } from "@chakra-ui/react"
+import { BellIcon, ChatIcon} from "@chakra-ui/icons"
 import { Icon } from '@chakra-ui/react'
 import { MdPermIdentity } from "react-icons/md";
 import ImgNavInicial from '/src/img/NavbarInicial.svg';
 import LogoBranca from '../../img/logorecdaterra.svg'
 
-function Navbar() {
+function NavbarSemPesquisa() {
     return (
         <div className='container'>
             <Flex w={"100%"} h={"150px"} p={'10'} backgroundImage={`url(${ImgNavInicial})`} alignItems={'center'} justifyContent={'space-around'}>
@@ -15,27 +15,7 @@ function Navbar() {
                         <img src={LogoBranca} alt="Logo"></img>
                     </Link>
                 </Box>
-                <Box w={'466px'} m={'20px'}>
-                    <InputGroup alignItems={'center'}>
-                        <Input
-                            variant='outline'
-                            type='search'
-                            placeholder='Pesquise Produtos, Clientes, Produtores...'
-                            backgroundColor={'#D6CFC5'}
-                            fontWeight='light'
-                            border={'none'}
-                            borderRadius={'30px'}
-                            boxShadow='7px 7px 10px rgba(0, 0, 0, 0.1)'
-                        />
-                        <InputRightElement backgroundColor={'#D6CFC5'}>
-                        <Link as={RouterLink} to="#">
-                                <IconButton variant="unstyled" borderRadius={'30px'}>
-                                    <SearchIcon color='black' boxSize={5}/>
-                                </IconButton>
-                            </Link>
-                        </InputRightElement>
-                    </InputGroup>
-                </Box>
+                
                 <Box display={'flex'} m={'20px'} color={'white'}>
                     <Text fontSize='xl'>
                         <Link pl={'10px'} pr={'10px'} as={RouterLink} to="/produtos">Produtos</Link>
@@ -63,4 +43,4 @@ function Navbar() {
     )
 }
 
-export default Navbar;
+export default NavbarSemPesquisa;
