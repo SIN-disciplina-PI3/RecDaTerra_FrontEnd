2985d793427cb8b70790d8631c1e4ecea7177174
Gabriel Henrique Lins <130785312+gabrielhilins@users.noreply.github.com>
2024-04-26 15:08:10 -0300

    adicionando o register do swiper no main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ChakraProvider } from '@chakra-ui/react';
+import { register } from 'swiper/element/bundle'
+
+register();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
