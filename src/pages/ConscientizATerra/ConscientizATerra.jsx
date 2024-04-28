4d8464265beaaa4c485a7e30001c571e089ca864
Gabriel Henrique Lins <130785312+gabrielhilins@users.noreply.github.com>
2024-04-27 23:54:14 -0300

    atualizando o swiper pt1
    colocando a logica, conteudo generico e deixando quase certo

--- src/pages/ConscientizATerra/ConscientizATerra.jsx
+++ src/pages/ConscientizATerra/ConscientizATerra.jsx
@@ -1,9 +1,10 @@
 import './ConscientizATerra.css';
 import NavbarSemPesquisa from '../../components/NavbarSemPesquisa/NavbarSemPesquisa';
 import Footer from '../../components/Footer/Footer';
-import { Flex, Box, Center, Text } from '@chakra-ui/react';
+import { Flex, Box, Center, Text, Image } from '@chakra-ui/react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
+import 'swiper/css/effect-cube';
 
 function ConscientizAterra() {
     return (
@@ -21,27 +22,55 @@ function ConscientizAterra() {
                     <Text as="p" color={'white'} opacity={0.9} fontSize={14} maxW={700} textAlign={'justify'} mt={3}>Aqui você vai encontrar as melhores informações, notícias, curiosidades e dicas sobre o mundo da sustentabilidade, agricultura, artesanato, nutrição, higiene, entre outras conhecimentos que ajudam a ConscientizAr a nossa Terra</Text>
                 </Center>
 
-                <Box w="100%" h="1300px" display="flex" justifyContent="center" alignItems="center">
+                <Box w="100%" h="auto" display="flex" justifyContent="center" alignItems="center" pt={70}>
                     <Swiper
-                        slidesPerView={1}
+                        slidesPerView={3}
                         spaceBetween={30}
-                        loop={false} // Desativar o loop
+                        centeredSlides={true}
+                        loop={true} 
                         pagination={{
                             clickable: true,
                         }}
-                        navigation={true}
+                        navigation={{
+                            nextEl: '.swiper-button-next',
+                            prevEl: '.swiper-button-prev',
+                        }}
                         className="mySwiper"
+                        style={{ width: '100%', maxWidth: '1200px', height: '600px' }}
+                        breakpoints={{
+                            768: {
+                                slidesPerView: 1,
+                            },
+                        }}
                     >
-                        <SwiperSlide>Slide 1</SwiperSlide>
-                        <SwiperSlide>Slide 2</SwiperSlide>
-                        <SwiperSlide>Slide 3</SwiperSlide>
-                        <SwiperSlide>Slide 4</SwiperSlide>
-                        <SwiperSlide>Slide 5</SwiperSlide>
-                        <SwiperSlide>Slide 6</SwiperSlide>
-                        <SwiperSlide>Slide 7</SwiperSlide>
-                        <SwiperSlide>Slide 8</SwiperSlide>
-                        <SwiperSlide>Slide 9</SwiperSlide>
+                        <SwiperSlide>
+                            <Box bg="#7ED957" color="white" p={4} borderRadius="20px" width="550px" height="550px" display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start">
+                                <Text textAlign="center" mt={3}>Texto do Slide 1</Text>
+                                <Center>
+                                    <Image src="imagem1.jpg" alt="Imagem 1" />
+                                </Center>
+                            </Box>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <Box bg="#7ED957" color="white" p={4} borderRadius="20px" width="550px" height="550px" display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start">
+                                <Text textAlign="center" mt={3}>Texto do Slide 2</Text>
+                                <Center>
+                                    <Image src="imagem2.jpg" alt="Imagem 2" />
+                                </Center>
+                            </Box>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <Box bg="#7ED957" color="white" p={4} borderRadius="20px" width="550px" height="550px" display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start">
+                                <Text textAlign="center" mt={3}>Texto do Slide 3</Text>
+                                <Center>
+                                    <Image src="imagem3.jpg" alt="Imagem 3" />
+                                </Center>
+                            </Box>
+                        </SwiperSlide>
+                        {/* Adicione os demais slides da mesma forma */}
                     </Swiper>
+                    <div className="swiper-button-prev" style={{ color: '#98FF68', padding: '40px' }}></div>
+                    <div className="swiper-button-next" style={{ color: '#98FF68', padding: '40px' }}></div>
                 </Box>
 
             </Flex>
