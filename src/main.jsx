8761109976abec623ef5c7c82bf5536d60bb72f3
Gabriel Henrique Lins <130785312+gabrielhilins@users.noreply.github.com>
2024-05-04 16:56:42 -0300

    fazendo o modal do Produtor e ajeitando o footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client'; 
+import App from './App.jsx';
 import { ChakraProvider } from '@chakra-ui/react';
-import { register } from 'swiper/element/bundle'
+import { register } from 'swiper/element/bundle';
 
 register();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
-    <App />
-    </ChakraProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
   </React.StrictMode>,
-)
+);
