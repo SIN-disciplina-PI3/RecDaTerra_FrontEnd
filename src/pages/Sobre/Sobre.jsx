70d14c1e9019c3a542caa28ad3e9d2689518891c
Gabriel Henrique Lins <130785312+gabrielhilins@users.noreply.github.com>
2024-05-04 22:06:07 -0300

    corrigindo erro do Navbar do sobre
    Sobre concluído

--- src/pages/Sobre/Sobre.jsx
+++ src/pages/Sobre/Sobre.jsx
@@ -1,10 +1,10 @@
-import NavbarSemPesquisa from "../../components/NavbarSemPesquisa/NavbarSemPesquisa";
+
 import { Box, Center, Flex, Text } from "@chakra-ui/react";
 
 function Sobre() {
     return (
         <div className="container">
-            <NavbarSemPesquisa />
+            
             <Flex direction="column" align="center">
                 <Box className="sobre-container" maxW="900px" textAlign="justify" mx="auto" p="20px" mt={100}>
                     <Center mb="20px">
