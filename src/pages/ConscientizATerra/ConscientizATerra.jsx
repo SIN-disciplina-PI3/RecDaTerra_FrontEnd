21163af26dd6ee6d783b14cfb0d800c87e0cd583
Gabriel Henrique Lins <130785312+gabrielhilins@users.noreply.github.com>
2024-05-10 15:09:10 -0300

    Atualizando ConscientizATerra.jsx
    Faltando apenas colocar o conteudo dentro dos slides

--- src/pages/ConscientizATerra/ConscientizATerra.jsx
+++ src/pages/ConscientizATerra/ConscientizATerra.jsx
@@ -1,27 +1,22 @@
-import './ConscientizATerra.css';
 import { Flex, Box, Center, Text, Image } from '@chakra-ui/react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
-import 'swiper/css/effect-cube';
 
 function ConscientizAterra() {
     return (
         <div className="container">
-
             <Flex direction="column" align="center">
                 <Center>
                     <div className="title">
                         <h1>ConscientizATerra</h1>
                     </div>
                 </Center>
-
                 <Center mb="20px">
                     <Text as="p" color={'white'} opacity={0.9} fontSize={14} maxW={700} textAlign={'justify'} mt={3}>Aqui você vai encontrar as melhores informações, notícias, curiosidades e dicas sobre o mundo da sustentabilidade, agricultura, artesanato, nutrição, higiene, entre outras conhecimentos que ajudam a ConscientizAr a nossa Terra</Text>
                 </Center>
-
-                <Box w="100%" h="auto" display="flex" justifyContent="center" alignItems="center" pt={70}>
+                <Box w="90%" h="auto" display="flex" justifyContent="center" alignItems="center" pt={70}>
                     <Swiper
-                        slidesPerView={3}
+                        slidesPerView={3} 
                         spaceBetween={30}
                         centeredSlides={true}
                         loop={true} 
@@ -33,14 +28,8 @@ function ConscientizAterra() {
                             prevEl: '.swiper-button-prev',
                         }}
                         className="mySwiper"
-                        style={{ width: '100%', maxWidth: '1200px', height: '600px' }}
-                        breakpoints={{
-                            768: {
-                                slidesPerView: 1,
-                            },
-                        }}
                     >
-                        <SwiperSlide>
+                        <SwiperSlide style={{display: 'flex', justifyContent: 'center'}}>
                             <Box bg="#7ED957" color="white" p={4} borderRadius="20px" width="550px" height="550px" display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start">
                                 <Text textAlign="center" mt={3}>Texto do Slide 1</Text>
                                 <Center>
@@ -48,7 +37,7 @@ function ConscientizAterra() {
                                 </Center>
                             </Box>
                         </SwiperSlide>
-                        <SwiperSlide>
+                        <SwiperSlide style={{display: 'flex', justifyContent: 'center'}}>
                             <Box bg="#7ED957" color="white" p={4} borderRadius="20px" width="550px" height="550px" display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start">
                                 <Text textAlign="center" mt={3}>Texto do Slide 2</Text>
                                 <Center>
@@ -56,7 +45,7 @@ function ConscientizAterra() {
                                 </Center>
                             </Box>
                         </SwiperSlide>
-                        <SwiperSlide>
+                        <SwiperSlide style={{display: 'flex', justifyContent: 'center'}}>
                             <Box bg="#7ED957" color="white" p={4} borderRadius="20px" width="550px" height="550px" display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start">
                                 <Text textAlign="center" mt={3}>Texto do Slide 3</Text>
                                 <Center>
@@ -64,16 +53,38 @@ function ConscientizAterra() {
                                 </Center>
                             </Box>
                         </SwiperSlide>
-                        {/* Adicione os demais slides da mesma forma */}
-                    </Swiper>
-                    <div className="swiper-button-prev" style={{ color: '#98FF68', padding: '40px' }}></div>
-                    <div className="swiper-button-next" style={{ color: '#98FF68', padding: '40px' }}></div>
-                </Box>
+                        <SwiperSlide style={{display: 'flex', justifyContent: 'center'}}>
+                            <Box bg="#7ED957" color="white" p={4} borderRadius="20px" width="550px" height="550px" display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start">
+                                <Text textAlign="center" mt={3}>Texto do Slide 3</Text>
+                                <Center>
+                                    <Image src="imagem3.jpg" alt="Imagem 3" />
+                                </Center>
+                            </Box>
+                        </SwiperSlide>
+                        <SwiperSlide style={{display: 'flex', justifyContent: 'center'}}>
+                            <Box bg="#7ED957" color="white" p={4} borderRadius="20px" width="550px" height="550px" display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start">
+                                <Text textAlign="center" mt={3}>Texto do Slide 3</Text>
+                                <Center>
+                                    <Image src="imagem3.jpg" alt="Imagem 3" />
+                                </Center>
+                            </Box>
+                        </SwiperSlide>
+                        <SwiperSlide style={{display: 'flex', justifyContent: 'center'}}>
+                            <Box bg="#7ED957" color="white" p={4} borderRadius="20px" width="550px" height="550px" display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start">
+                                <Text textAlign="center" mt={3}>Texto do Slide 3</Text>
+                                <Center>
+                                    <Image src="imagem3.jpg" alt="Imagem 3" />
+                                </Center>
+                            </Box>
+                        </SwiperSlide>
 
+                    </Swiper>
+                        <div className="swiper-button-prev" style={{ color: '#98FF68', marginleft: '40px' }}></div>
+                        <div className="swiper-button-next" style={{ color: '#98FF68', marginRight: '10px' }}></div>
+                </Box>
             </Flex>
-            
         </div>
-    )
+    );
 }
 
 export default ConscientizAterra;
