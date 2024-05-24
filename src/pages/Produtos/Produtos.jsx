a9331e98ac9a3cf0b6b5c38e6c3da71b01fbfee7
Yuri Catunda <131727946+yuuricathugaa@users.noreply.github.com>
2024-05-24 18:01:43 -0300

    Update Produtos.jsx
    Atualizando a página de produtos

--- src/pages/Produtos/Produtos.jsx
+++ src/pages/Produtos/Produtos.jsx
@@ -1,4 +1,5 @@
-import { Flex, Button, Text, Image } from '@chakra-ui/react';
+import React from 'react';
+import { Flex, Button, Text, Image, Heading, Center } from '@chakra-ui/react';
 import Veg from '../../img/Veg.png'
 import Cosmetico from '../../img/Perfume.png'
 import Jardinagem from '../../img/Jardinagem.png'
@@ -13,132 +14,154 @@ import Artesanato from '../../img/Artesanato.png'
 function Produtos() {
     return (
         <div className="container">
-            <div className="title">
-                Catálogo de Produtos
-            </div>
-            <Flex justifyContent="center" flexWrap="wrap" mt={'60px'}>
+            <Center>
+                <Heading as="h1" textAlign="center" color="#98FF68" mt={8} mb={10}>
+                    Catálogo de Produtos
+                </Heading>
+            </Center>
+            <Flex justifyContent="center" flexWrap="wrap" flexDirection={{ base: 'column', md: 'row' }}>
                 <Flex
-                    display="flex"
                     flexDirection="column"
-                    marginRight={100} // Espaçamento horizontal entre as colunas
+                    alignItems="center"
+                    marginRight={[0, 0, 100]} // Espaçamento horizontal entre as colunas, responsivo
+                    marginBottom={8} // Espaçamento entre os blocos de botões
                 >
                     <Button
-                        marginBottom={10}
+                        marginBottom={4}
                         backgroundColor={'#F0DD32'}
                         borderRadius={15}
-                        width={"360px"}
+                        width={['calc(50% - 20px)', '360px']} // 50% do container menos o espaçamento para telas menores, 360px para telas maiores
                         height={'90px'}
-                        justifyContent={'center'} // Alinhar o conteúdo do botão à esquerda
-                        paddingLeft={4} // Espaçamento à esquerda do conteúdo do botão
+                        justifyContent={'center'}
+                        paddingLeft={4}
+                        display="flex"
+                        alignItems="center"
                     >
-                        <Text color={'black'} fontWeight={'bold'} marginRight={20}>Alimentos Veg</Text>
-                        <Image src={Veg} alt='icone veg' ml={20}></Image>
+                        <Text color={'black'} fontWeight={'bold'} marginRight={4}>Alimentos Veg</Text>
+                        <Image src={Veg} alt='icone veg' />
                     </Button>
                     <Button
-                        marginBottom={10}
+                        marginBottom={4}
                         backgroundColor={'#E29FAB'}
                         borderRadius={15}
-                        width={"360px"}
+                        width={['calc(50% - 20px)', '360px']} // 50% do container menos o espaçamento para telas menores, 360px para telas maiores
                         height={'90px'}
-                        justifyContent={'center'} // Alinhar o conteúdo do botão à esquerda
+                        justifyContent={'center'}
+                        display="flex"
+                        alignItems="center"
                     >
-                        <Text color={'white'} fontWeight={'bold'} marginRight={20}>Cosméticos</Text>
-                        <Image src={Cosmetico} alt='icone cosmeticos' ml={20}></Image>
+                        <Text color={'white'} fontWeight={'bold'} marginRight={4}>Cosméticos</Text>
+                        <Image src={Cosmetico} alt='icone cosmeticos' />
                     </Button>
                     <Button
-                        marginBottom={10}
+                        marginBottom={4}
                         backgroundColor={'#FFFFFF'}
                         borderRadius={15}
-                        width={"360px"}
+                        width={['calc(50% - 20px)', '360px']} // 50% do container menos o espaçamento para telas menores, 360px para telas maiores
                         height={'90px'}
-                        justifyContent={'center'} // Alinhar o conteúdo do botão à esquerda
+                        justifyContent={'center'}
+                        display="flex"
+                        alignItems="center"
                     >
-                        <Text color={'black'} fontWeight={'bold'} marginRight={20}>Jardinagem</Text>
-                        <Image src={Jardinagem} alt='icone jardinagem' ml={20}></Image>
+                        <Text color={'black'} fontWeight={'bold'} marginRight={4}>Jardinagem</Text>
+                        <Image src={Jardinagem} alt='icone jardinagem' />
                     </Button>
                     <Button
-                        marginBottom={10}
+                        marginBottom={4}
                         backgroundColor={'#6957D9'}
                         borderRadius={15}
-                        width={"360px"}
+                        width={['calc(50% - 20px)', '360px']} // 50% do container menos o espaçamento para telas menores, 360px para telas maiores
                         height={'90px'}
-                        justifyContent={'center'} // Alinhar o conteúdo do botão à esquerda
+                        justifyContent={'center'}
+                        display="flex"
+                        alignItems="center"
                     >
-                        <Text color={'white'} fontWeight={'bold'} marginRight={20}>Limpeza</Text>
-                        <Image src={Limpeza} alt='icone limpeza' ml={20}></Image>
+                        <Text color={'white'} fontWeight={'bold'} marginRight={4}>Limpeza</Text>
+                        <Image src={Limpeza} alt='icone limpeza' />
                     </Button>
                     <Button
-                        marginBottom={10}
+                        marginBottom={4}
                         backgroundColor={'#8A9784'}
                         borderRadius={15}
-                        width={"360px"}
+                        width={['calc(50% - 20px)', '360px']} // 50% do container menos o espaçamento para telas menores, 360px para telas maiores
                         height={'90px'}
-                        justifyContent={'center'} // Alinhar o conteúdo do botão à esquerda
+                        justifyContent={'center'}
+                        display="flex"
+                        alignItems="center"
                     >
-                        <Text color={'white'} fontWeight={'bold'} marginRight={20}>Reciclados</Text>
-                        <Image src={Reciclados} alt='icone reciclados' ml={20}></Image>
+                        <Text color={'white'} fontWeight={'bold'} marginRight={4}>Reciclados</Text>
+                        <Image src={Reciclados} alt='icone reciclados' />
                     </Button>
                 </Flex>
                 <Flex
-                    display="flex"
                     flexDirection="column"
-                    marginLeft={100} // Espaçamento horizontal entre as colunas
+                    alignItems="center"
+                    marginLeft={[0, 0, 100]} // Espaçamento horizontal entre as colunas, responsivo
                 >
                     <Button
-                        marginBottom={10}
+                        marginBottom={4}
                         backgroundColor={'#D9AC7E'}
                         borderRadius={15}
-                        width={"360px"}
+                        width={['calc(50% - 20px)', '360px']} // 50% do container menos o espaçamento para telas menores, 360px para telas maiores
                         height={'90px'}
-                        justifyContent={'center'} // Alinhar o conteúdo do botão à esquerda
+                        justifyContent={'center'}
+                        display="flex"
+                        alignItems="center"
                     >
-                        <Text color={'white'} fontWeight={'bold'} marginRight={20}>Artesanato</Text>
-                        <Image src={Artesanato} alt='icone artesanato' ml={20}></Image>
+                        <Text color={'white'} fontWeight={'bold'} marginRight={4}>Artesanato</Text>
+                        <Image src={Artesanato} alt='icone artesanato' />
                     </Button>
                     <Button
-                        marginBottom={10}
+                        marginBottom={4}
                         backgroundColor={'#95E0F8'}
                         borderRadius={15}
-                        width={"360px"}
+                        width={['calc(50% - 20px)', '360px']} // 50% do container menos o espaçamento para telas menores, 360px para telas maiores
                         height={'90px'}
-                        justifyContent={'center'} // Alinhar o conteúdo do botão à esquerda
+                        justifyContent={'center'}
+                        display="flex"
+                        alignItems="center"
                     >
-                        <Text color={'white'} fontWeight={'bold'} marginRight={20}>Higiene</Text>
-                        <Image src={Higiene} alt='icone higiene' ml={20}></Image>
+                        <Text color={'white'} fontWeight={'bold'} marginRight={4}>Higiene</Text>
+                        <Image src={Higiene} alt='icone higiene' />
                     </Button>
                     <Button
-                        marginBottom={10}
+                        marginBottom={4}
                         backgroundColor={'#D34B10'}
                         borderRadius={15}
-                        width={"360px"}
+                        width={['calc(50% - 20px)', '360px']} // 50% do container menos o espaçamento para telas menores, 360px para telas maiores
                         height={'90px'}
-                        justifyContent={'center'} // Alinhar o conteúdo do botão à esquerda
+                        justifyContent={'center'}
+                        display="flex"
+                        alignItems="center"
                     >
-                        <Text color={'white'} fontWeight={'bold'} marginRight={20}>Legumes</Text>
-                        <Image src={Legumes} alt='icone legumes' ml={20}></Image>
+                        <Text color={'white'} fontWeight={'bold'} marginRight={4}>Legumes</Text>
+                        <Image src={Legumes} alt='icone legumes' />
                     </Button>
                     <Button
-                        marginBottom={10}
+                        marginBottom={4}
                         backgroundColor={'#D42121'}
                         borderRadius={15}
-                        width={"360px"}
+                        width={['calc(50% - 20px)', '360px']} // 50% do container menos o espaçamento para telas menores, 360px para telas maiores
                         height={'90px'}
-                        justifyContent={'center'} // Alinhar o conteúdo do botão à esquerda
+                        justifyContent={'center'}
+                        display="flex"
+                        alignItems="center"
                     >
-                        <Text color={'white'} fontWeight={'bold'} marginRight={20}>Moda</Text>
-                        <Image src={Moda} alt='icone moda' ml={20}></Image>
+                        <Text color={'white'} fontWeight={'bold'} marginRight={4}>Moda</Text>
+                        <Image src={Moda} alt='icone moda' />
                     </Button>
                     <Button
-                        marginBottom={10}
+                        marginBottom={4}
                         backgroundColor={'#80A73A'}
                         borderRadius={15}
-                        width={"360px"}
+                        width={['calc(50% - 20px)', '360px']} // 50% do container menos o espaçamento para telas menores, 360px para telas maiores
                         height={'90px'}
-                        justifyContent={'center'} // Alinhar o conteúdo do botão à esquerda
-
+                        justifyContent={'center'}
+                        display="flex"
+                        alignItems="center"
                     >
-                        <Text color={'white'} fontWeight={'bold'} marginRight={20}>Verduras</Text>
-                        <Image src={Verdura} alt='icone verdura' ml={20}></Image>
+                        <Text color={'white'} fontWeight={'bold'} marginRight={4}>Verduras</Text>
+                        <Image src={Verdura} alt='icone verdura' />
                     </Button>
                 </Flex>
             </Flex>
@@ -146,4 +169,4 @@ function Produtos() {
     );
 }
 
-export default Produtos;
+export default Produtos;
